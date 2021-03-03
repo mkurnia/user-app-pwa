99cdc204a2f581028186cbc20190a11e86049082
Muhammad Kurnia <muhammad.kurnia@stickearn.com>
2021-03-03 13:58:38 +0700

    1.0.2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,9 @@
     <header>
       <h3>Detail Vending Machine</h3>
     </header>
-    <main>
+    <content>
       <div class="pwa-card">
-        <div class="row">
+        <div class="pwa-content-box">
           <div class="app-address">
             <h3>D'Arcici Hotel Sunter</h3>
             <p>Jl. Sunter Permai Raya No.A1, RT.2/RW.12, Sunter Agung, Paradise, Kota Jkt Utara, Daerah Khusus Ibukota Jakarta 14350</p>
@@ -13,18 +13,22 @@
           </div>
         </div>
       </div>
-      <div class="content">
-        <div class="row">
+      <div class="pwa-content-box">
+        <div class="row space-between">
           <div class="pwa-col-6" v-for="item in 10" :key="item">
-            <div class="pwa-card">
-              <img src="https://cf.shopee.co.id/file/976f89461021e6d837aae1332a3f1bb1" alt="chitato">
-              <p>Chitato</p>
-              <p>Rp 10.500</p>
-            </div>
+            <router-link to="/">
+              <div class="pwa-card mb-20 mt-0">
+                <img src="https://cf.shopee.co.id/file/976f89461021e6d837aae1332a3f1bb1" alt="chitato">
+                <div class="p-20">
+                  <p class="mb-0 mt-0">Chitato</p>
+                  <p class="price-red mt-0">Rp 10.500</p>
+                </div>
+              </div>
+            </router-link>
           </div>
         </div>
       </div>
-    </main>
+    </content>
   </div>
 </template>
 
