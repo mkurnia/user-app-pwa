929b6f53accdf5584b57e9f7bff81bc13a826b40
Muhammad Kurnia <muhammad.kurnia@stickearn.com>
2021-03-10 00:25:29 +0700

    vue config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,3 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
 };
