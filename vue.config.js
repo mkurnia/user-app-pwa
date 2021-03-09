9ad9a9cee5e1788c9c1557a08a2f73a1b337b9d6
Muhammad Kurnia <muhammad.kurnia@stickearn.com>
2021-03-10 00:52:15 +0700

    no message

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "./",
+
+  pwa: {
+    name: 'Touchless Catalog',
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#42b983"
+    }
+  }
+};
