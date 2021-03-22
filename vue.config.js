4b72bd5dd67c1da22854de1cab515067adb8d354
Muhammad Kurnia <muhammad.kurnia@stickearn.com>
2021-03-22 09:31:10 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,11 @@ module.exports = {
 
   pwa: {
     name: "Touchless Catalog",
-    themeColor: "#42b983",
-    msTileColor: "#42b983",
+    themeColor: "#FFFFFF",
+    msTileColor: "#FFFFFF",
     appleMobileWebAppCache: "yes",
     manifestOptions: {
-      background_color: "#42b983"
+      background_color: "#FFFFFF"
     }
   },
 
